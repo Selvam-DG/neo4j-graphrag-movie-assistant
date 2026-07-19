693d4d81f06a249a0006d750afa4f7f78e3f1da9
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-07-19 09:59:32 +0000

    feat: completed project files through learning

--- graphrag_app/vector_rag.py
+++ graphrag_app/vector_rag.py
@@ -5,6 +5,8 @@ load_dotenv()
 from neo4j import GraphDatabase
 from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.generation import GraphRAG
 
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
@@ -15,10 +17,10 @@ driver = GraphDatabase.driver(
     )
 )
 
-# Create embedder
+#  embedder
 embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-# Create retriever
+# retriever
 retriever = VectorRetriever(
     driver,
     neo4j_database=os.getenv("NEO4J_DATABASE"),
@@ -27,11 +29,23 @@ retriever = VectorRetriever(
     return_properties=["title", "plot"],
 )
 
-# Create the LLM
+#  LLM
+llm = OpenAILLM(model_name="gpt-5.2")
 
-# Create GraphRAG pipeline
 
-# Search 
+#  GraphRAG pipeline
+rag = GraphRAG(retriever=retriever, llm=llm)
 
-# CLose the database connection
+# Search
+query_text = "Find me movies about toys coming alive"
+
+response = rag.search(
+    query_text=query_text, 
+    retriever_config={"top_k": 5}
+)
+
+print(response.answer)
+
+
+# Close the database connection
 driver.close()
